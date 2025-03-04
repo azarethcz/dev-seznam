4267cc8a2c6997c2309cd2f4b3ee2a0516342cf5
azarethcz <AZARETH1@seznam.cz>
2025-03-04 22:14:09 +0100

    Úprava skriptu pro automatizaci
    Bohužel jsem to načnu ale už asi nedodělám ale chtěl jsem aby se i vytočil automatizovaně celý ten dockerimage automatizovaně bez zadávání příkazu ale zjistil jsem že by to bylo na delší dobu zasílám Vám alespoň nástřel je třeba totiž měnit práva na složkách atd a nechci si to tím rozbít.

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker build -t devbuild . && docker run -p 9400:9400 -it --rm devbuild
+
+# Sestavení Docker image
+docker build -t devbuild .
+
+# Pokud sestavení proběhlo úspěšně, spusť kontejner
+if [ $? -eq 0 ]; then
+    echo "Docker image byl úspěšně sestaven. Spouštím kontejner..."
+    docker run -p 9400:9400 -it --rm devbuild
+else
+    echo "Chyba při sestavování Docker image!"
+    exit 1
+fi
